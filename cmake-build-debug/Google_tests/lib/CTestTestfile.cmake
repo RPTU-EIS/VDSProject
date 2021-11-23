e4df700f543c12dbf7dc37128db679db04b28bf8
BesjanTomja <besjan.tomja@fti.edu.al>
2021-11-23 20:49:27 +0100

    Adding Google Test Library

---
+++ cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/besitomja/Documents/BT GitHub/VDSProject-Group2/Google_tests/lib
+# Build directory: /Users/besitomja/Documents/BT GitHub/VDSProject-Group2/cmake-build-debug/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/src/test/CMakeFiles/VDSProject_test.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/besitomja/Documents/BT GitHub/VDSProject-Group2/src/test/main_test.cpp" "/Users/besitomja/Documents/BT GitHub/VDSProject-Group2/cmake-build-debug/src/test/CMakeFiles/VDSProject_test.dir/main_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/test"
+  "../Google_tests/lib/googletest/include"
+  "../Google_tests/lib/googletest"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/besitomja/Documents/BT GitHub/VDSProject-Group2/cmake-build-debug/src/CMakeFiles/Manager.dir/DependInfo.cmake"
+  "/Users/besitomja/Documents/BT GitHub/VDSProject-Group2/cmake-build-debug/Google_tests/lib/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/Users/besitomja/Documents/BT GitHub/VDSProject-Group2/cmake-build-debug/Google_tests/lib/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
