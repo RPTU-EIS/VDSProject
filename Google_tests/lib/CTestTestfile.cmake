a97924e1cb33e30d813b85c6b776ea9d77fddd55
JuledLikalla <juledlikalla8@gmail.com>
2021-11-23 21:00:40 +0100

    test

---
+++ Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/juled/Documents/C&C++ Workspace/C++ Workspace/VDSProject-Group2/Google_tests/lib
+# Build directory: C:/Users/juled/Documents/C&C++ Workspace/C++ Workspace/VDSProject-Group2/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/juled/Documents/C&C++ Workspace/C++ Workspace/VDSProject-Group2/Google_tests/lib/googletest
+# Build directory: C:/Users/juled/Documents/C&C++ Workspace/C++ Workspace/VDSProject-Group2/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
