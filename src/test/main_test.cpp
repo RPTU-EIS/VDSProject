2aded0f18d0372a55e68d42a47fee35470f0407e
hiandra <38166524+hiandra@users.noreply.github.com>
2021-12-10 18:24:44 +0100

    [Removed test file]
       - Deleted test_trial.cpp, which was created just to test
         googletest.

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -1,9 +1,24 @@
-//
-// Created by ludwig on 22.11.16.
-//
-
-
 #include "Tests.h"
+
+using testing::Eq;
+
+namespace {
+    class ClassDeclaration : public testing::Test {
+    public:
+        ClassName obj;
+        ClassDeclaration(){
+            obj;
+        }
+    };
+}
+
+TEST_F(ClassDeclaration, nameOfTheTest1){
+    ASSERT_EQ("","");
+}
+
+TEST_F(ClassDeclaration, nameOfTheTest2){
+    ASSERT_EQ("","1");
+}
 
 int main(int argc, char* argv[])
 {
