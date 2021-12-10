2edc429736568eb2fb66cfc2923ce1994dfc0d3f
hiandra <38166524+hiandra@users.noreply.github.com>
2021-12-10 20:45:48 +0100

    [Implementation of createVar]
       - Implemented function createVar() and both its
         unit tests passed.

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -12,6 +12,22 @@ Manager::Manager(){
 }
 
 BDD_ID Manager::createVar(const std::string &label) {
+    int number_nodes_table;
+    bool var_already_registered = false;
+    number_nodes_table = uniqueTableSize();
+
+    for (int i=0; i <= number_nodes_table; i++) {
+        if (unique_table[i].label == label) {
+            var_already_registered = true;
+            break;
+        }
+    }
+    if (!var_already_registered) {
+        BDD_node new_var(number_nodes_table,1,number_nodes_table,number_nodes_table, (std::string&)label);
+        unique_table.push_back(new_var);
+        return new_var.id;
+    }
+
     return 0;
 }
 
--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -16,7 +16,7 @@ TEST_F(UniqueTableTests, uniqueTableSize_sizeok){
 TEST_F(UniqueTableTests, createVar_sizeok){
 
     BDD_ID table_size = managerTest.uniqueTableSize();
-    EXPECT_EQ(5,table_size);
+    EXPECT_EQ(6,table_size);
 }
 
 TEST_F(UniqueTableTests, createVar_itemsok){
