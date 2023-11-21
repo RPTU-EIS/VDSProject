d9a0104370f59bb3c8251f05537e60947d272ca9
Kamel Fakih <kamelfakihh@gmail.com>
2023-11-21 12:11:51 +0100

    high, low, and topVar functions

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,1 +1,38 @@
 #include "Manager.h"
+
+namespace ClassProject {
+
+/**
+* @brief returns the id of the top variable of the given node f
+* @author Kamel Fakih
+* 
+* @param f
+* @return BDD_ID
+*/
+BDD_ID OBDDManager::topVar(BDD_ID f){
+    return nodes[f].data.topVar;
+}
+
+/**
+* @brief returns the id of the high node of the given node f
+* @author Kamel Fakih
+* 
+* @param f
+* @return BDD_ID
+*/
+BDD_ID OBDDManager::low(BDD_ID f){
+    return nodes[f].data.low;
+}
+
+/**
+* @brief returns the id of the low of the given node f
+* @author Kamel Fakih
+* 
+* @param f ID
+* @return BDD_ID
+*/
+BDD_ID OBDDManager::high(BDD_ID f){
+    return nodes[f].data.high;
+}
+
+}
