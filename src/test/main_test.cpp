4bdfd56e7e3f59fe2a404aa2d8cd7d0feeff14d4
Kamel Fakih <kamelfakihh1@gmail.com>
2023-12-06 03:18:22 +0100

    removed tests

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -10,25 +10,3 @@ int main(int argc, char* argv[])
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
-// TEST(TestTrue, TestReturn) {
-
-//     ClassProject::OBDDManager testClass = ClassProject::OBDDManager();    
-//     ASSERT_EQ(testClass.True(), 1);     
-// }
-
-// TEST(Testfalse, TestReturn) {
-
-//     ClassProject::OBDDManager testClass = ClassProject::OBDDManager();    
-//     ASSERT_EQ(testClass.False(), 0);  
-// }
-
-
-TEST(TestIsConstant, TestReturn) {
-
-    ClassProject::OBDDManager testClass = ClassProject::OBDDManager();    
-
-    ASSERT_EQ(testClass.isConstant(0), true);  
-    ASSERT_EQ(testClass.isConstant(1), true);  
-    ASSERT_EQ(testClass.isConstant(2), false);  
-}
