6a22c916c24dd85629a99b9c98ec4bf76da14af9
Hatice Seremet <haticesrmt52@gmail.com>
2023-12-12 11:24:45 -0500

    gettopvarnameTest

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -223,11 +223,11 @@ TEST_F(managerTest, topVarTest)
     // checks whether the top variable name for the BDD node with ID 6 is "c".
     ASSERT_EQ(manager.getTopVarName(5), "a");
 }
-TEST_F(managerTest, GetTopVarNameTest) {
-    andNode();
+/*TEST_F(managerTest, GetTopVarNameTest) {
+    ClassProject::BDD_ID f = manager.and2(a,b);
     //ASSERT_EQ(manager.topVar(b), b) << "Top variable of 'b' should be 'b' itself.";
-    ASSERT_EQ(manager.getTopVarName(andNode()), "a");
-}
+    ASSERT_EQ(manager.getTopVarName(f), "a");
+}*/
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
