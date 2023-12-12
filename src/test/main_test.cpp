9aacf8dc436a9e59b02c986cfede726f5d16a854
Hatice Seremet <haticesrmt52@gmail.com>
2023-12-12 12:29:41 -0500

    reachablenodes

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -233,14 +233,14 @@ TEST_F(managerTest, GetTopVarNameTest) {
 TEST_F(managerTest, findNodesTest)
 {
     ClassProject::BDD_ID f = manager.and2(a,b);
-    std::set<ClassProject::BDD_ID> nodes_of_root;
-    manager.findNodes(f, nodes_of_root);
+    std::set<ClassProject::BDD_ID> nodes;
+    manager.findNodes(f, nodes);
 
     // Check if the set contains all nodes of the expression
-    ASSERT_TRUE(nodes_of_root.find(f) != nodes_of_root.end());
-    ASSERT_TRUE(nodes_of_root.find(a) != nodes_of_root.end());
-    ASSERT_TRUE(nodes_of_root.find(b) != nodes_of_root.end());
-    // ASSERT_EQ(nodes.size(), expected_size);
+    /*ASSERT_TRUE(nodes.find(f) != nodes.end());
+    ASSERT_TRUE(nodes.find(a) != nodes.end());
+    ASSERT_TRUE(nodes.find(b) != nodes.end());*/
+    ASSERT_EQ(nodes.size(), 3);
 }
 
 
