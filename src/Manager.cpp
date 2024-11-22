d479f91bed7fb3c3a3e817f861b14fa7bb6ca59b
Elias Biehl <biehl@rptu.de>
2024-11-22 15:13:43 +0100

    added init_unique_tb function

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,1 +1,20 @@
 #include "Manager.h"
+
+
+
+namespace ClassProject {
+    Manager::Manager() {
+        init_unique_tb();
+    }
+
+    void Manager::init_unique_tb() {
+        uTableRow false_node = {.id=uniqueTableSize(), .high=False(), .low=False(), .topVar=False(), .label="false"};
+        unique_tb.push_back(false_node);
+        uTableRow true_node = {.id=uniqueTableSize(), .high=True(), .low=True(), .topVar=True(), .label="true"};
+        unique_tb.push_back(true_node);
+    }
+
+
+      
+}
+
