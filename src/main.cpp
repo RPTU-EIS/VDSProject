5642356cbd469f5ae6dc68c4472888bc68355c1d
Furkan Aktas <f_rkan_aktas@proton.me>
2024-12-03 12:49:04 +0100

    Add example ROBDD generation

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,18 @@
 #include <string>
 #include "Manager.h"
 
-
-
 int main(int argc, char* argv[])
 {
     std::cout << "Hello world :)" << std::endl;
+    using namespace ClassProject;
+    Manager m = Manager();
+
+    BDD_ID a = m.createVar("a");
+    BDD_ID b = m.createVar("b");
+    BDD_ID c = m.createVar("c");
+    BDD_ID d = m.createVar("d");
+
+    BDD_ID f = m.and2(m.or2(a, b), m.and2(c, d));
+
+    m.visualizeBDD("ROBDD.dot", f);
 }
